6f5704426dc6edae35580eb6169aecd21494e85c
AV <ValAnt@yandex.ru>
2024-04-04 12:28:36 +0300

    izmen3

--- 6 LABA/6 LABA/6 LABA.cpp
+++ 6 LABA/6 LABA/6 LABA.cpp
@@ -8,7 +8,7 @@ int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	int A[20];
+	int B[20];
 	int k1;
 	int sum = 0;
 
@@ -17,7 +17,7 @@ int main() {
 	k1 = rand() % 10 + 1;
 
 	printf("Массив A:\n");
-	for (int* ptr = A; ptr < &A[19]; ptr++) {
+	for (int* ptr = B; ptr < &B[19]; ptr++) {
 		*ptr = rand() % 201 - 100;
 		printf("%d ", *ptr);
 		if (*ptr > 0 && *ptr % k1 == 0) {
@@ -31,41 +31,41 @@ int main() {
 }
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <Windows.h>
-
-int main() {
-
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-
-	int M[8][8];
-	int maxRow = 0;
-	int maxElement = M[0][0];
-	int c = 1;
-
-	int* final = M[0] + 8 * 8;
-
-	srand(time(NULL));
-
-	printf("Двумерный массив M (сгенерированный):\n");
-	for (int* ptr = M[0], i = 1; ptr < final; ptr++, i++) {
-		*ptr = rand() % 201 - 100;
-		printf("%d\t", *ptr);
-		if (*ptr > maxElement) {
-			maxElement = *ptr;
-			maxRow = c;
-		}
-		if (i % 8 == 0) {
-			printf("\n");
-			c++;
-		}
-	}
-
-	printf("Наибольший элемент: %d\n", maxElement);
-	printf("Наибольший элемент находится в строке с номером: %d\n", maxRow);
-
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//#include <Windows.h>
+//
+//int main() {
+//
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+//
+//	int M[8][8];
+//	int maxRow = 0;
+//	int maxElement = M[0][0];
+//	int c = 1;
+//
+//	int* final = M[0] + 8 * 8;
+//
+//	srand(time(NULL));
+//
+//	printf("Двумерный массив M (сгенерированный):\n");
+//	for (int* ptr = M[0], i = 1; ptr < final; ptr++, i++) {
+//		*ptr = rand() % 201 - 100;
+//		printf("%d\t", *ptr);
+//		if (*ptr > maxElement) {
+//			maxElement = *ptr;
+//			maxRow = c;
+//		}
+//		if (i % 8 == 0) {
+//			printf("\n");
+//			c++;
+//		}
+//	}
+//
+//	printf("Наибольший элемент: %d\n", maxElement);
+//	printf("Наибольший элемент находится в строке с номером: %d\n", maxRow);
+//
+//	return 0;
+//}
